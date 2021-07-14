5f4473344176f7f69da7f982e052301cc274ca20
Asihati Hazaiti <asihati.hazaiti@Asihatis-MacBook-Pro.local>
2021-07-14 14:27:23 +0800

    [asihati] feat.: add a database

--- src/main/kotlin/demo/DemoApplication.kt
+++ src/main/kotlin/demo/DemoApplication.kt
@@ -2,10 +2,16 @@ package demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.relational.core.mapping.Table
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
-import java.util.*
+import org.springframework.data.annotation.Id
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 @SpringBootApplication
 class DemoApplication
@@ -14,19 +20,36 @@ fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
 
-
-data class Message(val id:String?, val text: String)
+@Table("MESSAGES")
+data class Message(@Id val id:String?, val text: String)
 data class Book(val id: String?,val name:String, val author : String, val writeAt:LocalDate)
 
+interface MessageRepository : CrudRepository<Message,String>{
+
+	@Query("select * from messages")
+	fun findMessages():List<Message>
+}
+@Service
+class MessageService(val db: MessageRepository){
+
+	fun findMessages():List<Message> = db.findMessages()
+
+	fun post(message: Message){
+		db.save(message)
+	}
+}
+
+
 @RestController
-class MessageResource{
+class MessageResource(val service: MessageService){
 	@GetMapping
-	fun index():List<Message> = listOf(
-		Message("1","Hello!"),
-		Message("2","Bonjour"),
-		Message("3","privet"),
-		Message(null,"Salem"),
-	)
+	fun index():List<Message> = service.findMessages()
+
+	@PostMapping
+	fun post(@RequestBody message: Message){
+		service.post(message)
+	}
+
 	@GetMapping("/books")
 	fun getBooks():List<Book> = listOf(
 		Book("1","Kotlin in practice","Asqat",LocalDate.now()),
